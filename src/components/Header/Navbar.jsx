ff37403ad4796fc39cf5a3872deda128ddfb8bca
Mainul-J <mainulh06@gmail.com>
2026-08-13 02:31:18 +0600

    set up environment of ract,tailwind css ,daisy ui and React Router also

---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1 className='text-center text-2xl'>nav bar</h1>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1 className="text-7xl text-center w-9/12 mx-auto my-52 text-blue-500 border p-5">page not found</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
