a545019d5bb26c082fcb31e90ba7b69d7a57b46f
Mainul-J <mainulh06@gmail.com>
2026-08-13 02:48:24 +0600

    navbar 7 footer  copy form daisy ui  & coustomize by me

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-3xl text-center">home pages or home section</h1>
+        </div>
+    );
+};
+
+export default Home;
