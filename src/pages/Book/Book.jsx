42a5423e8b2f9978d8cce24b8c7b5743ccc9dfbb
Mainul-J <mainulh06@gmail.com>
2026-08-21 02:35:01 +0600

    implement Dynamic routs & 'params , useParams Hook and fetching data'

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,12 +1,15 @@
 import React, { use } from "react";
+import { FaStarHalfAlt } from "react-icons/fa";
+import { Link } from "react-router";
 
 const Book = ({ book }) => {
   const { bookName,review
-, bookId, author, image } = book;
+, bookId, author, image,category,rating } = book;
   // const data =use(bookPromise);
   // console.log(data);
   return (
-    <div className="card bg-base-100 w-80 shadow-2xl">
+    <Link to={`/bookDetails/${bookId}`}>
+        <div className="card bg-base-100 w-80 shadow-2xl">
   <figure className="p-6 rounded-2xl w-2/3 mx-auto m-10 bg-amber-100 rounded ">
     <img
       src={image}
@@ -14,8 +17,10 @@ const Book = ({ book }) => {
       className="rounded-xl h-[166px] w-auto" />
   </figure>
   <div className="card-body items-center text-center text-slate-700 bg-amber-200 ">
-    <div className="">
+    <div className="flex justify-around bg-white py-2 px-10 rounded-2xl gap-30">
       {/* author name  */}
+      <div className="badge badge-outline">{category}</div>
+      <div className="badge badge-outline">{rating}</div>
     </div>
     <h2 className="card-title">{bookName}</h2>
     <p className="items-center flex">{(review).slice(0,200)}</p>
@@ -24,6 +29,7 @@ const Book = ({ book }) => {
     </div>
   </div>
 </div>
+    </Link>
   );
 };
 
--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const BookDetails = () => {
+    const {id} = useParams();
+    const data = useLoaderData()
+    const bookId = parseInt(id);  
+    const singleBook = data.find(book=>book.bookId === bookId);
+    const {bookName,author,image,review,tags} = singleBook
+    console.log(singleBook,typeof bookId,data);
     return (
-        <div>
-            
+        <div className=' border p-2 rounded-2xl my-10'>
+            <div className="flex items-center bg-red-200 rounded-xl">
+            <div className="rounded-2xl w-1/2 bg-slate-400">
+                <img className='w-full  py-5 px-4' src={image} alt="" />
+            </div>
+            <div className="w-1/2 ml-20">
+                <h5 className="text-xl font-semibold my-2">{bookName}</h5>
+                <h1 className='text-2xl font-bold my-2'>{author}</h1>
+                <p className='w-6/12'>{review}</p>
+                <div className='flex gap-5 my-5'>
+                    {tags.map((tag) => <button className='btn' key={bookId}>{tag}</button>)}
+                </div>
+            </div>
+            </div>
         </div>
+//         <div className="flex items-center justify-between gap-10 my-10 bg-red-200 rounded-xl">
+//     <div className="w-1/2 rounded-2xl bg-slate-400">
+//         <img
+//             className="w-full py-5 px-4"
+//             src={image}
+//             alt=""
+//         />
+//     </div>
+
+//     <div className="w-1/2">
+//         <h5 className="text-xl font-semibold my-2">{bookName}</h5>
+//         <p>{review}</p>
+//     </div>
+// </div>
     );
 };
 
